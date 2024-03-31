5e1828abcda1a03b2524b6faf39ff50fd3529241
Cornelius Linux <radi.adi78@gmail.com>
2024-03-31 16:10:15 +0700

    Styles, Layouts, Containers

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse reprehenderit sunt, animi est et laborum repellat modi eligendi eius?
+    </Link>
+  </div>
 }
