bd5327873532e39aebb714e43fa8bd0da9c07485
Cornelius Linux <radi.adi78@gmail.com>
2024-04-06 13:34:43 +0700

    Category component design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from "./featured.module.css"
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey,Leenoogs here!</b> Discover my stories and  creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" className={styles.image} alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit placeat incidunt soluta aliquam odit ipsum, iusto quo quisquam facilis earum error aliquid repellendus! Qui aliquid, pariatur ducimus recusandae, aliquam dolore sed saepe ad nemo at ex molestiae, quaerat incidunt. Voluptatum, consequatur sit vero iusto praesentium aperiam laborum amet magnam doloremque.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
