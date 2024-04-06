337bffe3356e16485152aa6b4cc58a2eaad3ae5b
Cornelius Linux <radi.adi78@gmail.com>
2024-04-06 13:53:26 +0700

    Blog post card component design

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,25 @@
+import styles from "./card.module.css"
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+
+export const Card = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+        </div>
+        <div className={styles.textContainer}>
+            <div className={styles.detail}>
+                <span className={styles.date}>11.02.2023 - </span>
+                <span className={styles.category}>CULTURE</span>
+            </div>
+            <Link href="">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+            </Link>
+            <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis sed rem possimus omnis error? Accusamus laboriosam consequuntur minima at ratione consectetur doloribus consequatur sequi...</p>
+            <Link href="/" className={styles.link}>Read More</Link>
+        </div>
+    </div>
+  )
+}
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,11 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
+import { Card } from '../card/Card'
 import Pagination from '../pagination/Pagination'
 import styles from "./cardList.module.css"
 
 const CardList = () => {
   return (
     <div className={styles.container}>
-      CardList
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card/>
+      </div>
       <Pagination/>
     </div>
   )
