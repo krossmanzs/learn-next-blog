2e2fd1f6cfe9bb24f35b58f874de449a944f7a8b
Cornelius Linux <radi.adi78@gmail.com>
2024-04-13 01:59:41 +0700

    How to sign in with google

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,19 +3,20 @@
 import React, { useState } from 'react'
 import Link from 'next/link';
 import styles from "./authLinks.module.css"
+import { signOut, useSession } from 'next-auth/react';
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false)
 
-  // temporary
-  const status = "notauthenticated"
-  return <>
-    {status === "notauthenticated" ? (
+  const {status} = useSession()
+
+  return (<>
+    {status === "unauthenticated" ? (
       <Link href="/login" className={styles.link}>Login</Link>
     ) : (
       <>
         <Link href="/write" className={styles.link}>Write</Link>
-        <span className={styles.links}>Logout</span>
+        <span className={styles.links} onClick={signOut}>Logout</span>
       </>
     )}
     <div className={styles.burger} onClick={()=>setOpen(!open)}>
@@ -38,7 +39,7 @@ const AuthLinks = () => {
           )}
         </div>
     )}
-  </>
+  </>)
 };
 
 export default AuthLinks;
