975856613760ee05f47799b3b8b0f4e0375f2dce
Cornelius Linux <radi.adi78@gmail.com>
2024-04-17 13:16:34 +0700

    Prisma blog app example

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,14 +7,17 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
-export default function Home() {
+export default function Home({searchParams}) {
+
+  const page = parseInt(searchParams.page) || 1;
+
   return (
     <div>
       <div className={styles.container}>
         <Featured/>
         <CategoryList/>
         <div className={styles.content}>
-          <CardList/>
+          <CardList page={page}/>
           <Menu/>
         </div>
       </div>
